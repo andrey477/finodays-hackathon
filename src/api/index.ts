6c497d7fc383726da54b8fa94df3bc437716ff86
Андрей Сушков <andrejsuskov@MacBook-Air-Andrej.local>
2022-11-09 18:34:33 +0300

    netlify setting

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const api = axios.create({
   withCredentials: true,
   headers: {
+    Accept: 'application/json',
     'Content-Type': 'application/json',
   },
 });
